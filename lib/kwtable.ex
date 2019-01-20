e6ca732fbcd7a4c96f7427d1bf731124db9877a7
mtannaan <12782884+mtannaan@users.noreply.github.com>
2019-01-20 21:01:00 +0900

    elixlsx integration

--- lib/kwtable.ex
+++ lib/kwtable.ex
@@ -27,7 +27,7 @@ defmodule KWTable do
   @type row :: Keyword.t()
 
   @spec normalize(t, any) :: t
-  def normalize(table, fill \\ nil) do
+  def normalize(table, fillna \\ nil) do
     cols_by_rows = table |> Enum.map(&Keyword.keys/1)
 
     cols = cols_by_rows |> Stream.concat() |> Enum.uniq()
@@ -36,7 +36,7 @@ defmodule KWTable do
       raise ArgumentError, message: "duplicate column name is not supported yet"
     end
 
-    table |> Enum.map(fn row -> interpolate_row(row, cols, fill) end)
+    table |> Enum.map(fn row -> interpolate_row(row, cols, fillna) end)
   end
 
   @doc """
@@ -74,13 +74,78 @@ defmodule KWTable do
     iex> KWTable.interpolate_row([a: 1, c: 3], [:a, :b, :c, :d], 255)
     [a: 1, b: 255, c: 3, d: 255]
   """
-  def interpolate_row(row, cols, fill \\ nil) do
+  def interpolate_row(row, cols, fillna \\ nil) do
     cols
     |> Enum.map(fn col ->
       case Keyword.get_values(row, col) do
         [value] -> {col, value}
-        [] -> {col, fill}
+        [] -> {col, fillna}
       end
     end)
+  end
+
+  @doc """
+  check if a table is already normalized (i.e. each row has no missing column)
+
+  ## Examples
+    iex> KWTable.normalized?([[a: 1, b: 2], [a: 3, b: 4]])
+    true
+
+    iex> KWTable.normalized?([[a: 1, b: 2], [a: 3, c: 5]])
+    false
+  """
+  @spec normalized?(t) :: boolean
+  def normalized?(table) do
+    table |> Enum.dedup_by(&Keyword.keys/1) |> length() |> (fn x -> x in [0, 1] end).()
+  end
+
+  @doc """
+  Transforms keyword-based table to list-based,
+  whose first row is a header containing column names.
+  If fillna is not :raise, the table is normalized if not yet.
+
+  ## Examples
+    iex> KWTable.to_rows([[a: 1, b: 2], [a: 3, b: 4], [a: 5, b: 6]])
+    [["a", "b"], [1, 2], [3, 4], [5, 6]]
+
+    iex> KWTable.to_rows([[a: 1, b: 2], [a: 3], [a: 5, b: 6]], 255)
+    [["a", "b"], [1, 2], [3, 255], [5, 6]]
+  """
+  @spec to_rows(t, :raise | any) :: [[any]]
+  def to_rows(table, fillna \\ :raise) do
+    norm_table =
+      case {fillna, normalized?(table)} do
+        {:raise, false} -> raise ArgumentError, message: "input table is not normalized"
+        {_, false} -> table |> normalize(fillna)
+        {_, true} -> table
+      end
+
+    cols = hd(norm_table) |> Keyword.keys() |> Enum.map(&Kernel.to_string/1)
+    norm_table |> Enum.into([cols], &Keyword.values/1)
+  end
+
+  @doc """
+  converts table to an `Elixlsx.Sheet` struct
+  """
+  @spec to_sheet(t, sheet_opts) :: Elixlsx.Sheet.t()
+  @type sheet_opts :: [{:fillna, :raise | any} | {:sheet_name, String.t()}]
+  def to_sheet(table, opts \\ []) do
+    fillna = Keyword.get(opts, :fillna, :raise)
+    sheet_name = Keyword.get(opts, :sheet_name, "Sheet")
+    %Elixlsx.Sheet{name: sheet_name, rows: to_rows(table, fillna)}
+  end
+
+  @doc """
+  converts table to an `Elixlsx.Workbook` struct which has a single sheet.
+  """
+  @spec to_workbook(t, sheet_opts) :: Elixlsx.Workbook.t()
+  def to_workbook(table, opts \\ []) do
+    sheet = to_sheet(table, opts)
+    %Elixlsx.Workbook{sheets: [sheet]}
+  end
+
+  @spec to_xlsx(t, String.t(), sheet_opts) :: {:ok, String.t()} | {:error, any()}
+  def to_xlsx(table, file_name, opts \\ []) do
+    table |> to_workbook(opts) |> Elixlsx.write_to(file_name)
   end
 end
